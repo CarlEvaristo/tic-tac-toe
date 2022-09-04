22e54c3429179111ec6420e85f041000d341b631
CarlEverest78 <thepythoneer@gmail.com>
2022-09-04 23:44:26 +0200

    add styling and responsiveness

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,15 +1,18 @@
 import React from "react"
 import Board from "./Board"
+import Confetti from 'react-confetti'
 
 function Game() {
-    const [gameWon, setGameWon] = React.useState(false)
+    const [gameWon, setGameWon] = React.useState(0)
+    const [gameOver, setGameOver] = React.useState(false)
     const [squares, setSquares] = React.useState([0, 0, 0, 0, 0, 0, 0, 0, 0])
-    const [player1Turn, setPlayer1Turn] = React.useState(true)
+    const [playerTurn, setPlayerTurn] = React.useState(true)
 
     function newGame(){
         setSquares([0, 0, 0, 0, 0, 0, 0, 0, 0])
-        setGameWon(false)
-        setPlayer1Turn(true)
+        setGameWon(0)
+        setGameOver(false)
+        setPlayerTurn(true)
     }
 
     React.useEffect(() => {
@@ -19,42 +22,56 @@ function Game() {
         for (let index in horStart) {
             let id = (horStart[index])
             if ((squares[id] !== 0) && (squares[id+1] === squares[id]) && (squares[id+2] === squares[id])) {
-                setGameWon(true)
+                setGameWon([id, id+1, id+2])
             } 
         }
         for (let index in vertStart) {
             let id = (vertStart[index])
             if ((squares[id] !== 0) && (squares[id+3] === squares[id]) && (squares[id+6] === squares[id])) {
-                setGameWon(true)
+                setGameWon([id, id+3, id+6])
             } 
         }
         if ((squares[0] !== 0) && (squares[4] === squares[0]) && (squares[8] === squares[0])) {
-            setGameWon(true)
+            setGameWon([0,4,8])
         } 
         if ((squares[6] !== 0) && (squares[4] === squares[6]) && (squares[2] === squares[6])) {
-            setGameWon(true)
+            setGameWon([6,4,2])
         } 
 
-        setPlayer1Turn(prev => !prev)
-
+        if (!squares.includes(0) && !gameWon) {     
+            console.log(squares)
+            setGameOver(true)
+        }
     }, [squares])
 
 
     function handleClick(id) {
         setSquares(prev => (prev.map((square, index) => {
             if (index === id && square === 0) {
-                return player1Turn ? "X" : "O"
+                setPlayerTurn(prev => !prev)        
+                return playerTurn ? "x" : "o"
             } else {
                 return square
-            }
+            } 
         })))
     }
     
     return (
         <div className="game">  
-            <h1>{gameWon ? `Player ${player1Turn ? 1 : 2} wins` : player1Turn ? "Player 2 turn:" : "Player 1 turn:"}</h1>
-            <Board handleClick={handleClick} squares={squares} gameWon={gameWon} />
-            {gameWon && <button onClick={newGame} className="newGameBtn">New Game</button>}
+            {(gameWon !== 0) && <Confetti />}
+            <h1>Tic Tac Toe</h1>
+            {gameWon ? <h2>Player {playerTurn ? 2 : 1} wins </h2> :
+            gameOver ? <h2>Game Over</h2> :
+            playerTurn ? <h2>Player 1:</h2> : 
+            <h2>Player 2:</h2>
+            }
+  
+            <Board  handleClick={handleClick} squares={squares} gameWon={gameWon} />
+            {(gameWon || gameOver) && <button onClick={newGame} className="newGameBtn">New Game</button>}
+
+            {/* {gameWon ? <Board  handleClick={handleClick} squares={squares} gameWon={gameWon} /> :
+            gameOver ? <Board  handleClick={handleClick} squares={squares} gameWon={gameWon} /> :
+            <h2>choose a square</h2> */}
         </div>
     )   
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,13 @@
 import React from "react"
 
 function Square({onClick, value, index, gameWon}) {
-    return <button onClick={() => onClick(index)} className="square" disabled={gameWon}>{value === 0 ? "" : value}</button>
-}
+    return (
+    <button onClick={() => onClick(index)} 
+            className="square" 
+            disabled={gameWon}
+            style={{backgroundColor: (gameWon && gameWon.includes(index)) ? "#fdd521" : null } }>
+        {value === 0 ? "" : value}
+    </button>
+)}
 
 export default Square
